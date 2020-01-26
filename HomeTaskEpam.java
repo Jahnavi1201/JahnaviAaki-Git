fa86b06712426ee6b971cc050a395b9f8d69769b
Jahnavi Aaki <jahnavi.aaki@gmail.com>
2020-01-26 22:54:35 +0530

    First Commit

---
+++ HomeTaskEpam.java
@@ -0,0 +1,6 @@
+class HomeTaskEpam{
+   public static void main(String args[]){
+         System.out.println("My FirstHome Task of Epam");
+         System.out.println("Hello World");
+      }
+   }
